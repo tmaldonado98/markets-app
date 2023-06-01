50500f559eb38030c6d16a035c460ac8354e4b4e
Tomas Maldonado <tmaldonadotrs@gmail.com>
2023-06-01 12:36:58 -0700

    Getting workflow to trigger prod deployment.

--- next.config.js
+++ next.config.js
@@ -1,17 +1,25 @@
-// @ts-check
- 
-/**
- * @type {import('next').NextConfig}
- **/
 const nextConfig = {
-    exportPathMap: function () {
-      return {
-        '/': { page: '/' },
-        // Add other pages/routes here
-      };
-    },
-    experimental:{appDir: true},
-    reactStrictMode: true,
-  };
-   
-  module.exports = nextConfig;
+  // Other configurations...
+  async rewrites() {
+    return [
+      // Your rewrite rules here
+    ];
+  },
+  async generateBuildId() {
+    // Generate a custom build ID if needed
+    return 'your-build-id';
+  },
+  async generateStaticPaths() {
+    // Generate static paths if needed
+    return {
+      '/': { page: '/' },
+      // Add other pages/routes here
+    };
+  },
+  experimental: {
+    appDir: true,
+  },
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
