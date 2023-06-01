0af45379daea23e0cf4e9463d84e9f8c84ac0ca0
Tomas Maldonado <tmaldonadotrs@gmail.com>
2023-05-31 18:37:30 -0700

    Building out routes, and index component.

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,9 @@ export default function Home() {
 
       <Header/>
         <main>
-          Home daf
+        
+
+          <img style={{height: 'auto', width:'550px'}} src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F001%2F330%2F263%2Foriginal%2Fstock-market-graph-trading-chart-for-business-and-finance-free-vector.jpg&f=1&nofb=1&ipt=fa08a3632004a23da25f69703978ec9779c8e2777acb8f07786a6f2aad7b0bac&ipo=images"/>
 
         </main>
     
--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,6 +1,6 @@
 import { FC, ReactNode } from "react";
 import { Header, Footer } from "./components/Header";
-import './globals.css';
+// import './globals.css';
 // import { Footer } from "./components/Footer";
 
 interface LayoutProps {
