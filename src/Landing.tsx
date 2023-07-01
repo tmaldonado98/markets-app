08f737650c65e0466d1fbfb65d0e97df7e5daf56
Tomas Maldonado <tmaldonadotrs@gmail.com>
2023-06-30 17:11:19 -0700

    Created button to clear pinned items. Will move pinned from being sessionstorage to localstorage. Will create separate storage keys for each category: market index, news item, commodity, stock item, crypto item.

--- src/Landing.tsx
+++ src/Landing.tsx
@@ -6,6 +6,23 @@ import { MyContext } from './components/Context';
 import News from './components/News';
 import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 import { TiDelete } from 'react-icons/ti';
+import {BiHelpCircle} from 'react-icons/bi';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+} from '@chakra-ui/react'
+
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 
 export default function Landing() {
 
@@ -47,6 +64,31 @@ export default function Landing() {
     removePinnedItem(item)
   }
 
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  function clearPinned(){
+    for (let i = 0; i < sessionStorage.length; i++) {
+      const key = sessionStorage.key(i)!;
+    
+      if (key.includes('pinned')) {
+        sessionStorage.removeItem(key)
+
+      }
+    }
+    provokeUpdate(!update);
+    setTimeout(() => {
+      handleClose();
+    }, 500);
+  }
+
   return (
     <section id='landing-container'>
 
@@ -71,8 +113,9 @@ export default function Landing() {
                     <Heading size={'lg'}>Pinned items:</Heading> 
                     <div style={{display:'flex', gap:'12px', flexWrap:'wrap', justifyContent:'center'}}>
                       {parsedPinnedItems.map((each:string) => (
-                          <Card style={{width:'25%'}}>
+                          <Card style={{width:'25%'}} _hover={{transform: 'scale(1.05)', transition: 'ease-in-out 200ms'}} _active={{transform: 'scale(0.9)'}}>
                             <CardBody style={{display:'flex', flexDirection:'column', justifyContent:'center'}}>
+                              
                               <span style={{cursor:'pointer', alignSelf:'end'}} title='Unpin Item'>
                                 <TiDelete onClick={() => handleDelete(each)}/>
                               </span>
@@ -88,10 +131,34 @@ export default function Landing() {
                       You can pin your favorite items here. 
                     </Heading>
                   }
+                        {/* <BiHelpCircle /> */}
+                  <div>
+                    <Button onClick={handleClickOpen} _active={{transform: 'scale(0.9)'}}>
+                      Clear List
+                    </Button>
+
+                  </div>
                 </section>
               :
               ''  
               }
+                <Modal isOpen={open} onClose={handleClose}>
+                  <ModalOverlay />
+                  <ModalContent>
+                    <ModalHeader>Are You Sure?</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                      This action cannot be undone.
+                    </ModalBody>
+
+                    <ModalFooter style={{justifyContent:'center'}}>
+                      <Button colorScheme='blue' variant='ghost' mr={3} onClick={handleClose}>
+                        Close
+                      </Button>
+                      <Button colorScheme='red' variant='ghost' onClick={clearPinned}>Confirm</Button>
+                    </ModalFooter>
+                  </ModalContent>
+                </Modal>
 
           </div>
           <aside id='ad2'>
